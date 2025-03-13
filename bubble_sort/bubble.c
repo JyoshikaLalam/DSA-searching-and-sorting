294e704f8da6df7039ded7d112ab23a684670d71
JyoshikaLalam <133553926+JyoshikaLalam@users.noreply.github.com>
2025-03-13 21:26:23 +0530

    Update bubble.c

--- bubble_sort/bubble.c
+++ bubble_sort/bubble.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    int my_array[] = {64, 34, 25, 12, 22, 11, 90, 5};
+    int my_array[] = {64, 34, 45, 12, 22, 11, 90, 5};
     int n = sizeof(my_array) / sizeof(my_array[0]);
 
     for (int i = 0; i < n-1; i++) {
