7532bca5aa3c33f758dddb931be4fe5b57b36836
JyoshikaLalam <133553926+JyoshikaLalam@users.noreply.github.com>
2025-03-19 21:36:37 +0530

    Update binary.java

--- binary_search/binary.java
+++ binary_search/binary.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] myArray = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        int[] myArray = {1, 3, 5, 7, 19, 11, 13, 15, 17, 19};
         int myTarget = 15;
 
         int result = binarySearch(myArray, myTarget);
